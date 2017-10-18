dd3bddd75892ac7d596eab8a2af1f8be024fa670
ianagez <ianagez88@gmail.com>
2017-10-18 10:37:45 +0300

    currency added

--- server/modules/reportsPurchases-bata.js
+++ server/modules/reportsPurchases-bata.js
@@ -1,10 +1,11 @@
 var dataModel=require('../datamodel'), dirProducts=require('./dirProducts-bata');
 var wrh_pinvs= require(appDataModelPath+"wrh_pinvs"),
-    wrh_pinvs_products= require(appDataModelPath+"wrh_pinvs_products");
+    wrh_pinvs_products= require(appDataModelPath+"wrh_pinvs_products"),
+    sys_currency= require(appDataModelPath+"sys_currency");
 var dir_units= require(appDataModelPath+"dir_units"), dir_contractors= require(appDataModelPath+"dir_contractors");
 
 module.exports.validateModule = function(errs, nextValidateModuleCallback){
-    dataModel.initValidateDataModels([wrh_pinvs,wrh_pinvs_products, dir_units,dir_contractors], errs,
+    dataModel.initValidateDataModels([wrh_pinvs,wrh_pinvs_products,sys_currency, dir_units,dir_contractors], errs,
         function(){
             nextValidateModuleCallback();
         });
@@ -15,12 +16,15 @@ module.exports.init = function(app) {
     var repPurchasesReportTableColumns=[
         {"data": "ID", "name": "ID", "width": 100, "type": "text", visible:false },
         { dataSource:"wrh_pinvs" },
-        {"data": "SUPPLIER_NAME", "name": "Поставщик", "width": 100, "type": "text", /*visible:false,*/
-            dataSource:"dir_contractors", sourceField:"NAME", linkCondition:"dir_contractors.ID=wrh_pinvs.SUPPLIER_ID" },
         {"data": "UNIT_NAME", "name": "Подразделение", "width": 120, "type": "text",
             dataSource:"dir_units", sourceField:"NAME", linkCondition:"dir_units.ID=wrh_pinvs.UNIT_ID" },
+        {"data": "SUPPLIER_NAME", "name": "Поставщик", "width": 100, "type": "text", /*visible:false,*/
+            dataSource:"dir_contractors", sourceField:"NAME", linkCondition:"dir_contractors.ID=wrh_pinvs.SUPPLIER_ID" },
         {"data": "DOCDATE", "name": "Дата", dataSource:"wrh_pinvs", sourceField:"DOCDATE" },
         {"data": "DOCNUMBER", "name": "Номер накладной", dataSource:"wrh_pinvs", sourceField:"NUMBER" },
+        {"data": "CURRENCY_CODE", "name": "Валюта", "width": 50, "type": "text", dataSource:"sys_currency", sourceField:"CODE",
+            linkCondition:"wrh_pinvs.CURRENCY_ID=sys_currency.ID"},
+        {"data": "RATE", "name": "Курс валюты", "width": 60, "type": "numeric2", dataSource:"wrh_pinvs"},
         {"data": "POSITION", "name": "Позиция в накладной", dataFunction:"ROUND(POSIND)" },
         { "data": "BARCODE", "name": "Штрихкод", "width": 100, "type": "text", align:"center",
             dataSource:"dir_products", sourceField:"PBARCODE", linkCondition:"dir_products.ID=wrh_pinvs_products.PRODUCT_ID"},
@@ -29,7 +33,7 @@ module.exports.init = function(app) {
         {"data": "POSSUM", "name": "Сумма" }
     ];
     repPurchasesReportTableColumns=
-        dirProducts.addProductColumnsTo(repPurchasesReportTableColumns,8,{
+        dirProducts.addProductColumnsTo(repPurchasesReportTableColumns,10,{
             visibleColumns:{"PRINT_NAME":false,"PBARCODE":false}});
     app.get("/reports/purchases/getPurchases", function (req, res) {
         wrh_pinvs_products.getDataForDocTable({tableColumns:repPurchasesReportTableColumns,
@@ -42,12 +46,15 @@ module.exports.init = function(app) {
     });
     var repPurchasesByDatesTableColumns=[
         {"data": "DOCDATE", "name": "Дата", dataSource:"wrh_pinvs",sourceField:"DOCDATE" },
+        {"data": "CURRENCY_CODE", "name": "Валюта", "width": 50, "type": "text", dataSource:"sys_currency", sourceField:"CODE",
+            linkCondition:"wrh_pinvs.CURRENCY_ID=sys_currency.ID"},
+        {"data": "RATE", "name": "Курс валюты", "width": 60, "type": "numeric2", dataSource:"wrh_pinvs"},
         {"data": "PRICE", "name": "Цена" },
         {"data": "SQTY", "name": "Кол-во", dataFunction:{function:"sumIsNull", source:"wrh_pinvs_products", sourceField:"QTY"} },
         {"data": "SPOSSUM", "name": "Сумма", dataFunction:{function:"sumIsNull", source:"wrh_pinvs_products", sourceField:"POSSUM"} }
     ];
     repPurchasesByDatesTableColumns=
-        dirProducts.addProductAttrsColumnsTo(repPurchasesByDatesTableColumns,1,{ excludeColumns:{}});
+        dirProducts.addProductAttrsColumnsTo(repPurchasesByDatesTableColumns,3,{ excludeColumns:{}});
     app.get("/reports/purchases/getPurchasesByDates", function (req, res) {
         wrh_pinvs_products.getDataForDocTable({tableColumns:repPurchasesByDatesTableColumns,
                 identifier:repPurchasesByDatesTableColumns[0].data,
@@ -64,6 +71,9 @@ module.exports.init = function(app) {
             dataSource:"dir_units", sourceField:"NAME", linkCondition:"dir_units.ID=wrh_pinvs.UNIT_ID" },
         {"data": "DOCDATE", "name": "Дата", dataSource:"wrh_pinvs", sourceField:"DOCDATE" },
         {"data": "DOCNUMBER", "name": "Номер накладной", dataSource:"wrh_pinvs", sourceField:"NUMBER" },
+        {"data": "CURRENCY_CODE", "name": "Валюта", "width": 50, "type": "text", dataSource:"sys_currency", sourceField:"CODE",
+            linkCondition:"wrh_pinvs.CURRENCY_ID=sys_currency.ID"},
+        {"data": "RATE", "name": "Курс валюты", "width": 60, "type": "numeric2", dataSource:"wrh_pinvs"},
         {"data": "SUPPLIER_NAME", "name": "Поставщик", "width": 100, "type": "text", visible:false,
             dataSource:"dir_contractors", sourceField:"NAME", linkCondition:"dir_contractors.ID=wrh_pinvs.SUPPLIER_ID" },
         {"data": "POSITION", "name": "Позиция в накладной", dataFunction:"ROUND(POSIND)" },
@@ -72,10 +82,10 @@ module.exports.init = function(app) {
         {"data": "POSSUM", "name": "Сумма" }
     ];
     repPurchasesWithProdAttrReportTableColumns=
-        dirProducts.addProductColumnsTo(repPurchasesWithProdAttrReportTableColumns,7,{
+        dirProducts.addProductColumnsTo(repPurchasesWithProdAttrReportTableColumns,9,{
             visibleColumns:{"CODE":false,"NAME":false,"UM":false,"PRINT_NAME":false,"PBARCODE":false}});
     repPurchasesWithProdAttrReportTableColumns=
-        dirProducts.addProductAttrsColumnsTo(repPurchasesWithProdAttrReportTableColumns,7,{
+        dirProducts.addProductAttrsColumnsTo(repPurchasesWithProdAttrReportTableColumns,9,{
             visibleColumns:{}});
     app.get("/reports/purchases/getPurchasesWithProdAttr", function (req, res) {
         wrh_pinvs_products.getDataForDocTable({tableColumns:repPurchasesWithProdAttrReportTableColumns,
